a597d85fc78ecb280f898563a156c053b61f30b4
Jose de Freitas <joselocotote26@gmail.com>
2024-09-07 15:09:35 +0100

    Add main structure

--- src/app.py
+++ src/app.py
@@ -1,7 +1,16 @@
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    return """
+    <h1>Multibuscador de libros</h1>
+    <h2>v. 1.0.0</h2>
+    <form>
+        <label for="titulo">Título del libro:</label>
+        <input type="text" name="titulo">
+        <input type="submit" value="Buscar">
+    </form>
+    """
