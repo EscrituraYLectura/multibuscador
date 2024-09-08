9c2665723dad13ef540a14f40e916328461537f8
Jose de Freitas <joselocotote26@gmail.com>
2024-09-08 12:32:59 +0100

    Create template

--- src/app.py
+++ src/app.py
@@ -1,16 +1,7 @@
-from flask import Flask
-from markupsafe import escape
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return """
-    <h1>Multibuscador de libros</h1>
-    <h2>v. 1.0.0</h2>
-    <form>
-        <label for="titulo">Título del libro:</label>
-        <input type="text" name="titulo">
-        <input type="submit" value="Buscar">
-    </form>
-    """
+    return render_template("index.html")
