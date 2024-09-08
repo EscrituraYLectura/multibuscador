4aa655dd7c1615fa878e3d8cb323aaa8b80acd0a
Jose de Freitas <joselocotote26@gmail.com>
2024-09-08 14:23:54 +0100

    Write main functionality

--- src/app.py
+++ src/app.py
@@ -1,10 +1,28 @@
+import json
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/", methods=('GET', 'POST'))
-def index(titulo=None):
-    if request.method == 'POST':
-        titulo = request.form['titulo']
+@app.route("/", methods=("GET", "POST"))
+def index(title=""):
+    if request.method == "POST":
+        title = request.form["title"]
 
-    return render_template("index.html", busqueda=titulo)
+    return render_template("index.html", result=search(title))
+
+
+def search(title):
+    links = []
+
+    if title == "":
+        return ""
+
+    with open("data.json", "r") as json_read:
+        json_data = json.load(json_read)
+
+    for name, content in json_data.items():
+        links.append(
+            [content["url"].replace("|TITLEQUERYSPACE|", content["separator"].join(title.split())), name]
+        )
+
+    return links
