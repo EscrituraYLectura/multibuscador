e2c71e1d57d07a0ca4a2b174f31af2f2c6dbddc3
Jose de Freitas <joselocotote26@gmail.com>
2025-06-24 18:30:19 +0100

    Add icon (favicon)

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,9 @@ import '@/styles/global.css';
 export const metadata: Metadata = {
     title: 'Multibuscador | Escritura y Lectura',
     description: 'Sitio web oficial del servidor Escritura y Lectura.',
+    icons: {
+        icon: '/multibuscador/icon.ico',
+    },
 };
 
 export default function RootLayout({
